b5a28a98b10bcf11affdcaf8278350b6b8a592f2
bjarlung <beatrice.jarlung@gmail.com>
2018-05-03 15:54:52 +0200

    CRUD for Library database

--- src/com/jabb/jabbsee/config/WebConfiguration.java
+++ src/com/jabb/jabbsee/config/WebConfiguration.java
@@ -4,14 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @Configuration
 @EnableWebMvc
-@EnableMongoRepositories(basePackages = "com.jabb.jabbsee.repository")
+//@EnableMongoRepositories(basePackages = "com.jabb.jabbsee.repository")
 @Import(MongoConfiguration.class)
 @ComponentScan("com.jabb.jabbsee.controller")
 public class WebConfiguration extends WebMvcConfigurerAdapter {
--- src/com/jabb/jabbsee/model/Library.java
+++ src/com/jabb/jabbsee/model/Library.java
@@ -1,10 +1,20 @@
 package com.jabb.jabbsee.model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Library {
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Library implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	private List<Serie> seriesList;
+	private String owner;
 	
 	public Library() {
 		this.seriesList = new ArrayList<>();
@@ -22,6 +32,16 @@ public class Library {
 	public void setSeriesList(List<Serie> seriesList) {
 		this.seriesList = seriesList;
 	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	
+	
 
 	
 }
