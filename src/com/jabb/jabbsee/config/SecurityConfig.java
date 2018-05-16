ea38348c05cae0e32b6fb8dc73e548e2239fe413
anneli.bertlid@gmail.com <anneli.bertlid@gmail.com>
2018-05-16 12:15:36 +0200

    added method configure in class sercurityconfig

--- src/com/jabb/jabbsee/config/SecurityConfig.java
+++ src/com/jabb/jabbsee/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.jabb.jabbsee.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -23,5 +24,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+				http
+				.csrf().disable()
+				.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+				.httpBasic();
+				
+	}
+	
 
 }
